b1e130f79a1cc3c286a4ad22885cfdd5de81a94f
Kshitiz-13 <55119355+Kshitiz-13@users.noreply.github.com>
2019-09-19 22:01:14 +0530

    Update Code file.ino

--- Exp_1_LedFlasher/Code file.ino
+++ Exp_1_LedFlasher/Code file.ino
@@ -5,8 +5,8 @@ pinMode(13, OUTPUT);  //sets the digital pin as output
 
 void loop()
 {
-digitalWrite(13,HIGH);  //turns the LED on
-delay(1000);  //waits for a second
-digitalWrite(13,LOW);  //turns the LED off
-delay(1000);  //waits for a second
+digitalWrite(12,HIGH);  //turns the LED on
+delay(500);  //waits for a second
+digitalWrite(12,LOW);  //turns the LED off
+delay(500);  //waits for a second
 }

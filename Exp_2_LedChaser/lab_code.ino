92bd6b0d06f620940369c0d518974c397399d469
Kshitiz-13 <55119355+Kshitiz-13@users.noreply.github.com>
2019-10-13 23:11:35 +0530

    Update lab_code.ino

--- Exp_2_LedChaser/lab_code.ino
+++ Exp_2_LedChaser/lab_code.ino
@@ -1,1 +1,24 @@
+void setup() {
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(10,OUTPUT);
+  pinMode(11,OUTPUT);
+
+
+}
+
+void loop() {
+  digitalWrite(8,HIGH);
+  digitalWrite(8,LOW);
+  delay(100);
+  digitalWrite(9,HIGH);
+  digitalWrite(9,LOW);
+  delay(100);
+  digitalWrite(10,HIGH);
+  digitalWrite(10,LOW);
+  delay(100);
+  digitalWrite(11,HIGH);
+  digitalWrite(11,LOW);
+  delay(100);
+}
 

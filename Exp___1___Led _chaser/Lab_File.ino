a47047db750dc1b76aec0ff84ece4292cb173c3a
Kshitiz-13 <55119355+Kshitiz-13@users.noreply.github.com>
2019-10-13 22:49:40 +0530

    Update Lab_File.ino

--- Exp___1___Led _chaser/Lab_File.ino
+++ Exp___1___Led _chaser/Lab_File.ino
@@ -4,9 +4,7 @@ void setup() {
   pinMode(9,OUTPUT);
   pinMode(10,OUTPUT);
   pinMode(11,OUTPUT);
-  pinMode(12,OUTPUT);
-  pinMode(13,OUTPUT);
-  
+
 
 }
 
@@ -22,11 +20,5 @@ void loop() {
   delay(100);
   digitalWrite(11,HIGH);
   digitalWrite(11,LOW);
-  delay(100);
-  digitalWrite(12,HIGH);
-  digitalWrite(12,LOW);
-  delay(100);
-  digitalWrite(13,HIGH);
-  digitalWrite(13,LOW);
   delay(100);
 }

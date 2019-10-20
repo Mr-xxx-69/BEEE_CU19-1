e8767e4cbf470b004c5a4fea482f5b67bbcd20af
Kshitiz-13 <55119355+Kshitiz-13@users.noreply.github.com>
2019-10-20 21:46:46 +0530

    Update lab code.ino

--- Exp_5_Light controlled/lab code.ino
+++ Exp_5_Light controlled/lab code.ino
@@ -1,1 +1,21 @@
+int sensorPin = A0;  
+int sensorValue = 0;  
+
+void setup() {
+  pinMode(2, OUTPUT); 
+  Serial.begin(9600); 
+}
+
+void loop() {
+ 
+  sensorValue = analogRead(sensorPin);    
+  Serial.println(sensorValue); 
+  
+  if(sensorValue < 900) 
+  digitalWrite(2,HIGH); 
+  
+  else digitalWrite(2,LOW); 
+  
+  delay(100);                  
+}
 
